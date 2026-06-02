4e0508d63a84c1d0cb7c50dc747f6677c01f81c8
Faraz Rawala <farazhussain.developer@gmail.com>
2026-06-02 05:35:53 +0500

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -116,10 +116,10 @@ const App = () => {
           <Route path="/branch/edit/:id" element={<BranchEdit />} />
           <Route path="/accounts" element={<Accounts />} />
           <Route path="/accounts/balance-sheet" element={<BalanceSheetPage />} />
-          <Route path="/accounts/income-statement" element={<IncomeStatementPage />} />
+          <Route path="/income-statement" element={<IncomeStatementPage />} />
           <Route path="/accounts/payments" element={<PaymentManagementPage />} />
-          <Route path="/accounts/payment-receipts" element={<PaymentReceiptsList />} />
-          <Route path="/accounts/payment-receipts/edit/:id" element={<PaymentReceiptEditPage />} />
+          <Route path="/payment-receipts" element={<PaymentReceiptsList />} />
+          <Route path="/payment-receipts/edit/:id" element={<PaymentReceiptEditPage />} />
           <Route path="/accounts/edit/:id" element={<AccountsEdit />} />
           <Route path="/expenses" element={<ExpenseIndex />} />
           <Route path="/expenses/add" element={<ExpenseAdd />} />
@@ -207,10 +207,10 @@ const App = () => {
           <Route path="/branch/edit/:id" element={<BranchEdit />} />
           <Route path="/accounts" element={<Accounts />} />
           <Route path="/accounts/balance-sheet" element={<BalanceSheetPage />} />
-          <Route path="/accounts/income-statement" element={<IncomeStatementPage />} />
+          <Route path="/income-statement" element={<IncomeStatementPage />} />
           <Route path="/accounts/payments" element={<PaymentManagementPage />} />
-          <Route path="/accounts/payment-receipts" element={<PaymentReceiptsList />} />
-          <Route path="/accounts/payment-receipts/edit/:id" element={<PaymentReceiptEditPage />} />
+          <Route path="/payment-receipts" element={<PaymentReceiptsList />} />
+          <Route path="/payment-receipts/edit/:id" element={<PaymentReceiptEditPage />} />
           <Route path="/accounts/edit/:id" element={<AccountsEdit />} />
           <Route path="/expenses" element={<ExpenseIndex />} />
           <Route path="/expenses/add" element={<ExpenseAdd />} />
